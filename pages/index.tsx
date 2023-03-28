38e09e4510f567206f8809b8503c7f8d3bd1d8b6
nf-projects <90344282+nf-projects@users.noreply.github.com>
2023-03-27 21:45:38 -0700

    colored cells

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,51 @@ interface CellProps {
     updateValue: (index: number, value: number | null) => void;
 }
 
-function Cell(props: CellProps) {
+function SudokuCellElement(props: CellProps) {
+    // maps index -> color id
+    const colorBlindColors = [
+        0, 0, 0, 1, 1, 1, 2, 2, 2,
+        0, 0, 0, 1, 1, 1, 2, 2, 2,
+        0, 0, 0, 1, 1, 1, 2, 2, 2,
+        3, 3, 3, 4, 4, 4, 5, 5, 5,
+        3, 3, 3, 4, 4, 4, 5, 5, 5,
+        3, 3, 3, 4, 4, 4, 5, 5, 5,
+        6, 6, 6, 7, 7, 7, 8, 8, 8,
+        6, 6, 6, 7, 7, 7, 8, 8, 8,
+        6, 6, 6, 7, 7, 7, 8, 8, 8
+    ];
+
+    const normalColors = [
+        0, 0, 0, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 0, 0, 0,
+        1, 1, 1, 0, 0, 0, 1, 1, 1,
+        1, 1, 1, 0, 0, 0, 1, 1, 1,
+        1, 1, 1, 0, 0, 0, 1, 1, 1,
+        0, 0, 0, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 0, 0, 0
+    ];
+
+    // maps color id -> color name
+    const colorNames = [
+        "bg-red-500",
+        "bg-yellow-500",
+        "bg-green-500",
+        "bg-blue-500",
+        "bg-indigo-500",
+        "bg-purple-500",
+        "bg-pink-500",
+        "bg-gray-500",
+        "bg-black-500",
+    ];
+
+    // get the color id for this cell
+    const colorId = normalColors[props.index];
+
+    // get the color name for this cell
+    const colorName = colorNames[colorId];
+
     const [oldValue, setOldValue] = useState<number | null>(null);
 
     return (
@@ -17,16 +61,13 @@ function Cell(props: CellProps) {
             onChange={(e) => {
                 const value = parseInt(e.target.value);
 
-                // not a number
-                if (isNaN(value)) {
+                // make sure manually entered values are valid
+                if (isNaN(value) || value === 0) {
                     // set the value to the value stored in state (old value)
                     e.target.value = "";
                     props.updateValue(props.index, null);
                     return;
-                }
-
-                // make sure manually entered values are valid
-                if (value < 1 || value > 9) {
+                } else if (value < 1 || value > 9) {
                     // set the value to the value stored in state (old value)
                     e.target.value = String(oldValue || 0);
                     return;
@@ -35,22 +76,23 @@ function Cell(props: CellProps) {
                 setOldValue(value);
                 props.updateValue(props.index, value);
             }}
-            className="text-center border border-gray-300 bg-red-500 w-16 h-16 rounded-md"
+            className={"text-center border border-gray-300 text-4xl font-bold pl-4 w-20 h-20 rounded-md" + " " + colorName}
         />
     );
 }
 
-export default function Home() {
-    interface Cell {
-        index: number;
-        value: number | null;
-    }
+export interface SudokuCellData {
+    index: number;
+    value: number | null;
+}
 
-    const [board, setBoard] = useState<Cell[]>([]);
+export default function Home() {
+
+    const [board, setBoard] = useState<SudokuCellData[]>([]);
 
     // initialize the board on first render
     useEffect(() => {
-        let newBoard: Cell[] = [];
+        let newBoard: SudokuCellData[] = [];
         for (let i = 0; i < 81; i++) {
             newBoard.push({index: i, value: null});
         }
@@ -66,17 +108,18 @@ export default function Home() {
         setBoard(newBoard);
     }
 
+    // generate the <input> tags
     let inputElements = [];
     let count = 0;
     for (let i = 0; i < 9; i++) {
         let rowElements = [];
 
         for (let j = 0; j < 9; j++) {
-            rowElements.push(<Cell key={count} index={count}
-                                   updateValue={(index, value) => {
-                                       updateBoardValue(index, value);
-                                       console.log(board);
-                                   }}/>);
+            rowElements.push(<SudokuCellElement key={count} index={count}
+                                                updateValue={(index, value) => {
+                                                    updateBoardValue(index, value);
+                                                    console.log(board);
+                                                }}/>);
 
             count++;
         }
